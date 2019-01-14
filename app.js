67f7668d66219a3a664bc15d0e766e92a10b3d81
Tristan Seifert <tristan@tseifert.me>
2019-01-14 17:29:14 -0600

    basic sass/handlebars template

--- app.js
+++ app.js
@@ -3,31 +3,57 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+var hbs = require('express-handlebars');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+
+// sass for stylesheets
+var sassMiddleware = require('node-sass-middleware');
+
+app.use('/css',
+  sassMiddleware({
+    src: __dirname + '/sass', //where the sass files are
+    dest: __dirname + '/public/css', //where css should go
+    debug: true,
+
+    // stylesheets are at /css over HTTP
+    // prefix: '/css',
+
+    // include for Foundation
+    includePaths: [
+      'node_modules/foundation-sites/scss'
+    ]
+  })
+);
+
+// serve foundation JS
+app.use("/js/jquery.min.js", express.static(__dirname + '/node_modules/jquery/dist/jquery.min.js'));
+app.use("/js/foundation.min.js", express.static(__dirname + '/node_modules/foundation-sites/dist/js/foundation.min.js'));
+
+// serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// index page (the only page we serve, l0l)
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
